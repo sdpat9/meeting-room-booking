d1d8f091730e07be5a3286bbdb7a15f833619026
Sergey <sdpat07@gmail.com>
2026-02-25 09:11:43 +0300

    feat: add Room domain model

---
+++ src/Room.java
@@ -0,0 +1,51 @@
+public class Room {
+    private Long id;
+    private String name;
+    private int capacity;
+    private boolean active;
+
+    public Room(String name, int capacity, boolean active) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be positive");
+        }
+
+        this.name = name;
+        this.capacity = capacity;
+        this.active = active;
+    }
+
+    public void changeCapacity(int newCapacity) {
+        if (newCapacity <= 0) {
+            throw new IllegalArgumentException("Capacity must be positive");
+        }
+        
+        this.capacity = newCapacity;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", capacity=" + capacity +
+                ", active=" + active +
+                '}';
+    }
+}
